98afa569eb4b744677560ebaa294a44e4b2e0d9f
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-24 17:26:58 +0100

    Added NaN method for wrong input

--- js/script.js
+++ js/script.js
@@ -26,23 +26,26 @@ let priceForKm = 0.21;
 let price = userKm * priceForKm;
 let discount20 = price - (price * 20 / 100);
 let discount40 = price - (price * 40 / 100);
+let isNan = Number.isNaN(userKm, userAge)
+console.log('Non è un numero?',isNan)
+
+if (isNan == true) {
+    alert("Inserisci i dati corretamente! Premi F5")
+}
+
 
 if (userAge >= 65) {
-
-
     console.log('ha risparmiato', price - discount40);
     console.log('costo biglietto', price - (price * 40 / 100));
-
     document.getElementById('price').innerHTML += ' ' + (price - (price - discount40)).toFixed(2) + ' ' + '€';
     document.getElementById('discount').innerHTML += ' ' + (price - discount40).toFixed(2) + ' ' + '€';
 
 } else if (userAge <= 18) {
-
     document.getElementById('price').innerHTML += ' ' + (price - (price - discount20)).toFixed(2) + ' ' + '€';
     document.getElementById('discount').innerHTML += ' ' + (price - discount20).toFixed(2) + ' ' + '€';
     console.log('costo biglietto', price - (price * 20 / 100));
     console.log('ha risparmiato', price - discount20)
-
-} else
+} else {
     document.getElementById('price').innerHTML += ' ' + price.toFixed(2) + ' ' + '€';
     document.getElementById('discount').innerHTML += '0€';
+}
